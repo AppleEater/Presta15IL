013ca24b7e76fea3a16d512fe90044ae749e57e4
AppleEater <uaharoni@gmail.com>
2013-11-06 19:12:58 +0200

    Fixining Hebrew RTL settings
    mainly focus on rtl.css

--- shop/themes/default/modules/autoupgrade/translations/he.php
+++ shop/themes/default/modules/autoupgrade/translations/he.php
@@ -2,6 +2,7 @@
 
 global $_MODULE;
 $_MODULE = array();
+$_MODULE['<{autoupgrade}default>adminselfupgrade_5243e62512552a2cd6369e446bbf10f4'] = 'ביצועי השרת';
 $_MODULE['<{autoupgrade}default>adminselfupgrade_93cba07454f06a4a960172bbd6e2a435'] = 'כן';
 $_MODULE['<{autoupgrade}default>adminselfupgrade_bafd7322c6e97d25b6299b5d6fe8920b'] = 'לא';
 $_MODULE['<{autoupgrade}default>adminselfupgrade_38fb7d24e0d60a048f540ecb18e13376'] = 'שמור';
